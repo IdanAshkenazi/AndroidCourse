dd6451a20d4316a6aff4e0d9b7d60e4cc24b439a
ronm <ronm@DESKTOP-UQSE2I7>
2018-07-25 20:44:55 +0300

    added the scanner the array list until input "stop"

--- AndroidCourse/src/com/johnbryce/lesson/hw15/main.java
+++ AndroidCourse/src/com/johnbryce/lesson/hw15/main.java
@@ -1,10 +1,21 @@
 package com.johnbryce.lesson.hw15;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Insert String");
+		ArrayList<String> list = new ArrayList<String> ();
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		while(!input.equals("stop"))
+		{
+			list.add(input);
+			input = scanner.nextLine();
+		}
+			System.out.println(list.toString());	
 	}
 
 }
